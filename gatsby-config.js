93af4244859b39fb78f9841c310ff070881f83e6
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-03-06 19:27:42 +0000

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins:[
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content/`,
+      },
+    },
+    `gatsby-transformer-json`,
+    `gatsby-plugin-styled-components`
+  ]
+}
